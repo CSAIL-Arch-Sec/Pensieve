ae68ccea290f7e841461bf69d9502985aea697e2
Yuheng Yang <yuhengy@mit.edu>
2023-07-10 17:30:47 -0500

    [smallfix] recover some code that was deleted by mistake

--- result/Figure10/run_local.py
+++ result/Figure10/run_local.py
@@ -1,7 +1,27 @@
 
 import sys, os
 sys.path.append(os.getcwd())
-from run import get_experiment_list
+
+
+def get_experiment_list():
+  param_saved_params_list = ["spectre", "DoM", "invisiSpec", "GhostMinion"]
+  param_ROB_size_list = [2, 4, 8, 16]
+  param_CPU_simuCycle_list = [3, 4, 5, 6, 7, 8, 9, 10]
+
+  experiment_list = []
+  inputId = 0
+  for param_saved_params in param_saved_params_list:
+    for param_ROB_size in param_ROB_size_list:
+      for param_CPU_simuCycle in param_CPU_simuCycle_list:
+        inputParam = "++arg --param-debug-assert ++arg 0 " \
+                   + "++arg --param-saved-params ++arg %s " % param_saved_params \
+                   + "++arg --param-ROB-size ++arg %d " % param_ROB_size \
+                   + "++arg --param-CPU-simuCycle ++arg %d " % param_CPU_simuCycle
+
+        experiment_list.append({"inputParam": inputParam, "inputId": str(inputId)})
+        inputId += 1
+  print("[experiment_list]:", experiment_list)
+  return experiment_list
 
 
 if __name__ == "__main__":
